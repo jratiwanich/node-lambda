f3ac7730c5fc0e33d50420f41df1c72bb24e4bbb
Jak Ratiwanich <jak@triangleitc.com>
2021-02-28 18:09:46 -0800

    first code

---
+++ README.md
@@ -0,0 +1,14 @@
+# Basic SERVERLESS wth Lambda
+
+1. create a project:
+```
+serverless create --template aws-nodejs --path node-basic
+```
+2. modify the js code 
+3. deploy to aws lambda
+```
+serverless deploy --aws-profile your-aws-profile-name
+```
+
+## Documentation:
+1. https://github.com/serverless/serverless
---
+++ handler.js
@@ -0,0 +1,27 @@
+'use strict';
+
+module.exports.purchase = async (event) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'This is your purchase of $10.00',
+      input: event,
+    }),
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
+
+module.exports.refund = async (event) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'This is your refund of $5.00',
+      input: event,
+    }),
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
